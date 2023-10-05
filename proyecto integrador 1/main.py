0c7713c3b16cbbcad0a7442635013d72d5dc2b50
suany veronica antunes oliva <olivasuani201@gmail.com>
2023-10-05 09:09:35 -0600

    Agregado nuevamente el readme.md

---
+++ proyecto integrador 1/main.py
@@ -0,0 +1,13 @@
+"""
+Primera parte 
+Crear un reporsitorio de Github para el proyecto, en este
+reporsitorio realizara lo siguiente:
+1. Crear un archivo README.md con la descripcion del  
+proyecto escriba por ti, esta sera tu documentacion del proyecto.
+2. Crear el archivo main del proyecto.
+3. Pedir el nombre del jugador por teclado.
+4. Imprimir un mensaje de bienvenida con el nombre.
+"""
+
+nombre = input("Hola! Cual es tu nombre de jugador?")
+print("Hola", nombre, "Bienvenido")
